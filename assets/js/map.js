9c1182f48897abe72cfc0e661e7db006fa37fb5a
Sneha Sunny <snehasunny321@gmail.com>
2023-06-27 18:58:35 +0530

    Updates

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,8 +1,11 @@
 function myMap() {
-  var mapCanvas = document.getElementById("map");
+  var mapContainer = document.createElement("div");
+  mapContainer.setAttribute("id", "map");
+  document.body.appendChild(mapContainer);
+
   var mapOptions = {
     center: new google.maps.LatLng(51.5, -0.2),
     zoom: 10
   };
-  var map = new google.maps.Map(mapCanvas, mapOptions);
+  var map = new google.maps.Map(mapContainer, mapOptions);
 }
